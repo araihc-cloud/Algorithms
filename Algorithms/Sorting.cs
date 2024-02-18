b904d3cb9e40da52a65650cd83fa2af52f18050f
Chiara <barberachiara98@gmail.com>
2024-02-18 14:10:02 +0100

    Added insertion sort

---
+++ Algorithms/Sorting.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Algorithms.Algorithms
+{
+    public static class Sorting
+    {
+        /// <summary>
+        /// Method <c>InsertionSort</c> sorts an array of integers.
+        /// The array is divided in two parts: a sorted part, consisting of the first
+        /// element of the array, and an unsorted part, consisting of the rest of the array.
+        /// The next element from the unsorted part of the array is taken and inserted
+        /// in the correct position in the sorted part of the array.
+        /// This is an efficient algorithm for sorting a small number of elements.
+        /// </summary>
+        /// <param name="array">An array of integers.</param>
+        /// <returns>The sorted array.</returns>
+        public static int[] InsertionSort(int[] array)
+        {
+            if (array == null) throw new ArgumentNullException("array");
+
+            if (array.Length < 1) return array;
+
+            int key;
+            int j;
+            for (int i = 1; i < array.Length; i++)
+            {
+                key = array[i];
+                j = i - 1;
+
+                while (j >= 0 && array[j] > key)
+                {
+                    array[j+1] = array[j];
+                    j--;
+                }
+
+                array[j+1] = key;
+            }
+
+            return array;
+        }
+    }
+} 
--- Program.cs
+++ Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Algorithms
 {
     internal class Program
     {
+        private static void SelectSortingAlgorithm()
+        {
+            Console.WriteLine("Select a sorting algorithm:");
+            Console.WriteLine("1 -> Insertion sort");
+
+            string algorithm = Console.ReadLine();
+            switch (algorithm)
+            {
+                case "1":
+                    break;
+                default:
+                    throw new InvalidDataException("The selected algorithm is not valid.");
+            }
+        }
+
+        private static void SelectOperation()
+        {
+            Console.WriteLine("Select an operation:");
+            Console.WriteLine("1 -> Sorting");
+
+            string operation = Console.ReadLine();
+            switch (operation)
+            {
+                case "1":
+                    Console.WriteLine("You have selected Sorting.");
+                    SelectSortingAlgorithm();
+                    break;
+                default:
+                    throw new InvalidDataException("The selected operation is not valid.");
+            }
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello!");
+
+            SelectOperation();
         }
     }
 }
